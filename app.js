46bd63630e5ee3d513710e625617954e00fe31c8
Maximilian <maxkusch@gmx.de>
2016-11-04 10:53:03 +0100

    DB eingebunden. Connectionfehler beim zugriff auf den server.

--- app.js
+++ app.js
@@ -2,27 +2,40 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
-
-var db = mongojs('articlelist', ['articlelist']);
-
+var db = require('mongodb').MongoClient;
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.get('/articlelist', function (req, res) {
-    console.log('I received Get')
-    
-    db.articlelist.find(function(err, docs) {
-       console.log(docs);
-        res.json(docs);
-    });
-});
+// Connect to the db
+db.connect("mongodb://test:test@ds139187.mlab.com:39187/eis_test", function(err, db) {
+  if(err) {
+    return console.dir(err);
+  }else {
+    console.log("connected to database")
+  }
 
-app.post('/articlelist', function (req, res){
-   console.log(req.body); 
-    
-    db.articlelist.insert(req.body, function(err, doc){
-       res.json(doc);
+  var articlelist = db.collection('articlelist');
+
+  app.get('/articlelist', function (req, res) {
+    console.log('I received Get')
+
+    articlelist.find(function(err, docs) {
+      console.log(docs);
+      res.json(docs).send(200);
     });
+  });
+
+  app.post('/articlelist', function (req, res){
+    console.log(req.body);
+
+    articlelist.insert(req.body, function(err, doc){
+      if(err){
+        return console.dir(err);
+      }else{
+        res.json(doc);
+      }
+    });
+  });
 });
 
 app.listen(3000);
--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,27 +1,28 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',function($scope, $http){
-    console.log("Hello World Controller");
-    
-var refresh = function() {  
-$http.get('/articlelist').success(function(response) {
+  console.log("Hello World Controller");
+
+/*var refresh = function() {
+  console.log("test1");
+$http.get('http:/localhost:3000/articlelist').success(function(response) {
     console.log("I got DATA");
     $scope.articlelist = response;
     $scope.article = "";
 });
 };
-    
+
     refresh();
-    
-    $scope.addArticle = function() {
-      console.log($scope.article);  
-        $http.post('/articlelist', $scope.article).success(function(response){;
-        console.log(response); 
-        refresh();
-                                                                              
-                                                                            
+*/
+  $scope.addArticle = function() {
+    console.log($scope.article);
+    var config = {
+      headers : {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+      }
+    }
+    $http.post('http:/localhost:3000/articlelist', $scope.article).success(function(response){
+      console.log(response);
+      //refresh();
     });
-    
-    };
+  };
 }]);
-
-
